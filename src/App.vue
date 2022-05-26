b04561399dc6f0f0b272faacc464017673dac703
Luca Cataldo <35984346+lucacataldo@users.noreply.github.com>
2022-05-26 16:11:37 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Splash from "./components/Splash.vue";
+</script>
+
+<template>
+	<div>
+		<Splash />
+		<div>
+			<br>
+			<br>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+:root {
+	--light: #ffffff;
+	--dark: #0e3e4c;
+	--secondary: #236264;
+	--highlight: #fbed25;
+	--lowlight: #66a789;
+	--customFont: Nothing You Could Do;
+}
+
+body,
+html {
+	padding: 0px;
+	margin: 0px;
+	font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/Splash.vue
@@ -0,0 +1,93 @@
+<script setup>
+function playVid() {
+	document.getElementById("bgVid").play();
+}
+</script>
+
+<template>
+	<div>
+		<section class="splashCont">
+			<span>STOP THE 413</span>
+			<div class="cta">Learn Why</div>
+		</section>
+		<video
+			@click="playVid"
+			id="bgVid"
+			autoplay
+			muted
+			loop
+			src="../assets/bg_1.mp4"
+		></video>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.splashCont {
+	width: 100%;
+	height: 100vh;
+	position: relative;
+	top: 0px;
+	left: 0px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: var(--light);
+	background: url("../assets/dust.jpg");
+	animation: bg 0.5s infinite linear alternate-reverse;
+	background-size: cover;
+	mix-blend-mode: screen;
+	flex-direction: column;
+	backdrop-filter: blur(5px);
+
+	span {
+		font-family: var(--customFont);
+		font-weight: 900;
+		font-size: 20vh;
+		line-height: 20vh;
+		padding: 10px;
+		z-index: 5;
+	}
+}
+
+@keyframes bg {
+	0% {
+		background: url("../assets/dust.jpg");
+	}
+
+	50% {
+		background: url("../assets/dust2.jpg");
+	}
+
+	100% {
+		background: url("../assets/dust3.jpg");
+	}
+}
+
+.cta {
+	font-size: 3vh;
+	padding: 10px 20px;
+	border: solid 2px var(--light);
+	margin: 20px;
+	transition: all 0.3s ease;
+	font-weight: bold;
+	cursor: pointer;
+
+	&:hover {
+		color: var(--dark);
+		background: var(--highlight);
+		border: solid 2px var(--highlight);
+		padding: 10px 30px;
+	}
+}
+
+#bgVid {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100vh;
+	object-fit: cover;
+	z-index: -1;
+	filter: brightness(0.5);
+}
+</style>
